a42d78d1836705104bd199b4bdea3e4c67552553
Ojo-Oluwagbenga <57333233+Ojo-Oluwagbenga@users.noreply.github.com>
2023-02-10 06:03:56 +0100

    Initial Commit

--- app/Http/Middleware/AddHeaders.php
+++ app/Http/Middleware/AddHeaders.php
@@ -1,13 +1,11 @@
 <?php namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Routing\Middleware;
+// use Illuminate\Contracts\Routing\Middleware; 
 
 // If Laravel >= 5.2 then delete 'use' and 'implements' of deprecated Middleware interface.
-class AddHeaders implements Middleware
-{
-    public function handle($request, Closure $next)
-    {
+class AddHeaders{
+    public function handle($request, Closure $next){
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
 
